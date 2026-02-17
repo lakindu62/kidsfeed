b4d0453818467f7f62caedc4aa35bac57c86e127
AmzalFoumi <147682418+AmzalFoumi@users.noreply.github.com>
2026-02-17 18:19:23 +0530

    chore(frontend): setup complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
     <Routes>
       {/* Example routes */}
-      <Route path="/" element={<div>Home Page</div>} />
+      <Route path="/" element={<Home />} />
       <Route path="/about" element={<div>About Page</div>} />
     </Routes>
   );
